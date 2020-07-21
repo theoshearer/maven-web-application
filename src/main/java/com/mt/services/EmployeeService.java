43b4b116f1fbe777c8b33b99389e2ab170cd16d8
mylandmarktech <67545522+mylandmarktech@users.noreply.github.com>
2020-07-21 15:47:14 -0400

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,11 +22,11 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Name", "Landmark Technologies");
+		js.put("Calling Name", "mylandmark");
+		js.put("DOB", "May 1, 2005");
+		js.put("Hobbies", "Praying, Singing, Reading Technical Blogs,Teaching, Helping to Poor People..");
+		js.put("Places he like", "Africa, NA, Bali");
 
 		return js.toString();
 }
